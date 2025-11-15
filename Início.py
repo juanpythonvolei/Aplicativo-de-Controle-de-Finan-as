1c7cb33903e7a5393f722bbb83e26d69eed28b11
juanpythonvolei <juanzsalca@outlook.com>
2025-11-15 14:37:22 -0300

    Ajustes_iniciais_commit

--- Início.py
+++ Início.py
@@ -1,30 +1,40 @@
 import streamlit as st 
 from engine.users.creation_and_update import *
 from engine.users.Acess.user_acess import *
+from engine.Counts.Counts_manegement import query_user_by_count_owner_id
 
 if "usuario_logado" not in st.session_state:
       st.session_state.usuario_logado = None
 
-main_container = st.container(
-  border=True
-)
-main_container.title("Aplicativo de Controle de Finanças")
-created_user_login = main_container.text_input("Insira seu usuário")
-created_user_keyword = main_container.text_input("Insira sua senha",type="password")
-acess_container = main_container.container(
-  border=True,
-  horizontal=True
-)
-acess_button = acess_container.button("Acessar")
+if st.session_state.usuario_logado == None:
 
-if acess_button:
-  if created_user_login and created_user_keyword:
-    login = acess_user(user=created_user_login,password=created_user_keyword)
-    if login:
-      st.success(f"Acesso realizado com sucesso")
+  main_container = st.container(
+    border=True
+  )
+  main_container.title("Aplicativo de Controle de Finanças")
+  created_user_login = main_container.text_input("Insira seu usuário")
+  created_user_keyword = main_container.text_input("Insira sua senha",type="password")
+  acess_container = main_container.container(
+    border=True,
+    horizontal=True
+  )
+  acess_button = acess_container.button("Acessar")
+
+  if acess_button:
+    if created_user_login and created_user_keyword:
+      login = acess_user(user=created_user_login,password=created_user_keyword)
+      if login:
+        st.success(f"Acesso realizado com sucesso")
+      else:
+        st.error("Usuário ou senha incorretos")
+    elif not created_user_login:
+      st.error("Preeehca o campo de usuário")
     else:
-      st.error("Usuário ou senha incorretos")
-  elif not created_user_login:
-    st.error("Preeehca o campo de usuário")
-  else:
-    st.error("Preencha o campo de senha")
+      st.error("Preencha o campo de senha")
+
+else:
+  finalize_section = st.container(border=True)
+  finalize_section.title(f"{query_user_by_count_owner_id(st.session_state.usuario_logado).username}")
+  finalize_section.button("Finalizar Sessão")
+  if finalize_section:
+    st.session_state.usuario_logado = None
--- complements/counts/Visualização_Geral_das_Contas.py
+++ complements/counts/Visualização_Geral_das_Contas.py
@@ -5,51 +5,56 @@ def load_general_vision():
   general_vision_contanier = st.container(border=True)
   title_container = general_vision_contanier.container(horizontal=True)
   title_container.title("Visão Geral das Contas")
-  general_vision_contanier.divider()
-  subcontainer = general_vision_contanier.container(border=False,horizontal=True,horizontal_alignment="distribute",vertical_alignment="center")
-  date_based_vision = subcontainer.toggle("Visualizar por data de vencimento")
-  count_based_vision = subcontainer.toggle("Visualizar por Conta")
-  if date_based_vision and not count_based_vision:
-     load_general_Vision_per_date()
-  if count_based_vision and not date_based_vision:
-    load_see_history()
-  general_vision_contanier.divider()
-  if not date_based_vision and not count_based_vision:
-    add_new_count =subcontainer.popover("➕")
-    with add_new_count:
-      load_add_count()
-    try:
+  add_new_count =title_container.popover("➕")
+  with add_new_count:
+          load_add_count()
+ 
+  
+  try:
+      general_vision_contanier.divider()
+      subcontainer = general_vision_contanier.container(border=False,horizontal=True,horizontal_alignment="distribute",vertical_alignment="center")
       counts = load_count(user=st.session_state.usuario_logado)
+      date_based_vision = subcontainer.toggle("Visualizar por data de vencimento")
+      count_based_vision = subcontainer.toggle("Visualizar Contas Finalizadas")
+      if date_based_vision and not count_based_vision:
+        load_general_Vision_per_date()
+      if count_based_vision and not date_based_vision:
+        load_see_history()
+      general_vision_contanier.divider()
+      if not date_based_vision and not count_based_vision:
+        
+        try:
+          for item in counts:
+              item_container = st.container(border=True,horizontal=True,key=item.id*item.id**item.id*12.3,vertical_alignment="center")
+              name_container = item_container.container(border=False,key=f"{item.id*99.12}")
+              name_container.subheader(f"{item.description}")
+              name_container.info(f"""
+              Valor da Conta (Mês) : R$ {item.value}
 
-      for item in counts:
-          item_container = st.container(border=True,horizontal=True,key=item.id*item.id**item.id*12.3,vertical_alignment="center")
-          name_container = item_container.container(border=False,key=f"{item.id*99.12}")
-          name_container.subheader(f"{item.description}")
-          name_container.info(f"""
-          Valor da Conta (Mês) : R$ {item.value}
+              Quantidade de Parcelas: {item.divisions}
 
-          Quantidade de Parcelas: {item.divisions}
+              Data de Vencimento (Todo dia) : {item.payment_day}
 
-          Data de Vencimento (Todo dia) : {item.payment_day}
-
-    """)
-          options_container = item_container.container(border=False,horizontal=True,key=item.id*item.id**item.id*12.4)
-          settings_count_button = options_container.popover("⚙️")
-          with settings_count_button:
-            load_settings(item=item)
-          delete_count_button = options_container.popover("🗑️")
-          with delete_count_button:
-            load_delete(item=item)
-          pay_count_button = options_container.popover("💵")
-          with pay_count_button:
-            load_payment(item=item)
-          contas = load_payed_count_infos(count_id=item.id,user_id=st.session_state.usuario_logado)
-          if contas:
-            history_count_payed_button = options_container.popover("📑")
-            with history_count_payed_button:
-              load_payment_history(count_id=item.id,contas=contas)
-            info_count_button = options_container.popover("🔎")
-            with info_count_button:
-              load_infos(item.id)
-    except:
-       pass
+        """)
+              options_container = item_container.container(border=False,horizontal=True,key=item.id*item.id**item.id*12.4)
+              settings_count_button = options_container.popover("⚙️")
+              with settings_count_button:
+                load_settings(item=item)
+              delete_count_button = options_container.popover("🗑️")
+              with delete_count_button:
+                load_delete(item=item)
+              pay_count_button = options_container.popover("💵")
+              with pay_count_button:
+                load_payment(item=item)
+              contas = load_payed_count_infos(count_id=item.id,user_id=st.session_state.usuario_logado)
+              if contas:
+                history_count_payed_button = options_container.popover("📑")
+                with history_count_payed_button:
+                  load_payment_history(count_id=item.id,contas=contas)
+                info_count_button = options_container.popover("🔎")
+                with info_count_button:
+                  load_infos(item.id)
+        except:
+          pass
+  except:
+    st.warning("Atenção Crie uma conta para Iniciar")
